33e01056297e9b4a056421db71418c862ebfdd58
GustavoRodrigue <gustavorodriguesgra13@gmail.com>
2020-01-17 15:51:41 -0300

    Alterações

--- src/app/add-cliente/add-cliente.page.ts
+++ src/app/add-cliente/add-cliente.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-add-cliente',
@@ -7,9 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddClientePage implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
-
+  Cliente(){
+    this.router.navigate(['/clientes'])
+  }
 }
